e942e1235060983521e4aaedfd426ff6e1bd8878
FrenzMcJ0hns0n <francois.christophe.mazoyer@gmail.com>
2017-10-10 22:46:50 +0200

    Minor code update and added function descriptions

--- password_generator.py
+++ password_generator.py
@@ -4,38 +4,41 @@ import datetime
 import random
 from pathlib import Path
 
-
-def ExitOnFatalError():
+def ExitOnError():
+	""" Simply displays a message, then quit """
 	print ("The program will exit now. Press Enter to continue ...")
 	input()
 	sys.exit(0)
 
 def CheckFiles(file):
+	""" Checks if the specified file really exists in the current working directory """
 	characterFile = Path(file)
 	if characterFile.is_file() == False:
 		print ("Fatal error : File \"characters.txt\" not found !")
-		ExitOnFatalError()
-		
+		ExitOnError()
+
 def LoadCharacters(file):
+	""" Returns a string which contains the characters loaded from the file 'characters.txt' """
 	fileReader = open(file, "r")
 	string = ""
 	for char in fileReader:
 		string += char
 	if string == "":
 		print ("Fatal error : There isn't any character to load !")
-		ExitOnFatalError()
+		ExitOnError()
 	else:
 		return string
 
 def CheckUserInput():
+	""" Verifies if the user's input is matching expectations, then returns an integer """
 	userInput = 0
-	while 1:
+	while True:
 		try:
 			userInput = int(input("Desired length (2-20) : "))
 		except ValueError:
 			print("Error : Not an integer !")
 			print("-------------------------")
-			continue
+			#continue
 		else:
 			if userInput >= 2 and userInput <= 20:
 				return userInput
@@ -44,21 +47,22 @@ def CheckUserInput():
 				print("-------------------------")
 
 def WriteDateIntoFile(file):
+	""" Writes the current time and date in french format into the output file """
 	formattedDate = str(datetime.datetime.now().strftime('%d/%m/%Y-%Hh%M'))
-	dateWriter = open(file, "a")
-	dateWriter.write("\n===" + formattedDate + "===")
-	dateWriter.close()
+	with open(file, 'a') as dateWriter:
+		dateWriter.write("\n===" + formattedDate + "===")
 
 def BuildPassword(password_length, data):
+	""" Buils a password from 'data' using the length chosen by the user """
 	output = ""
-	for x in range (0, password_length): 
+	for x in range (0, password_length):
 		output += str(random.choice(data))
 	return output
 
 def WritePasswordIntoFile(file, text):
-	textWriter = open(file, "a")
-	textWriter.write("\n" + text)
-	textWriter.close()
+	""" Writes the built password into the output file """
+	with open(file, 'a') as textWriter:
+		textWriter.write("\n" + text)
 
 
 CheckFiles("characters.txt")
@@ -67,7 +71,7 @@ WriteDateIntoFile("text.txt")
 print ("Possible characters :\n" + characters)
 print("-------------------------")
 
-while 1:
+while True:
 	chosenLength = CheckUserInput()
 	password = BuildPassword(chosenLength, characters)
 	WritePasswordIntoFile("text.txt", password)
